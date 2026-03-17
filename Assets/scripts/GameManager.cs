7e4d68b5606d7930188810d5396e629da050cb41
RokasJ-1 <rokas.juska.8b@gmail.com>
2026-03-17 18:37:20 +0200

    Pridėta Pergalės sąlyga
    Užėmus visus fakultetus laimima, rodomas Win Screen, galima lygį pradėti iš naujo

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -3,8 +3,45 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject winPanel; // UI panel (vėliau)
+
+    private bool gameEnded = false;
+
+    void Update()
+    {
+        if (gameEnded) return;
+
+        CheckWinCondition();
+    }
+
     public void ResetLevel()
     {
+        Time.timeScale = 1f; // svarbu (kad po win veiktu)
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void CheckWinCondition()
+    {
+        NodePastatas[] nodes = FindObjectsOfType<NodePastatas>();
+
+        foreach (NodePastatas node in nodes)
+        {
+            if (node.owner != NodePastatas.OwnerType.Player)
+                return; // jei bent vienas ne player - dar nelaimėta
+        }
+
+        WinGame();
+    }
+
+    void WinGame()
+    {
+        gameEnded = true;
+
+        Debug.Log("WIN!");
+
+        Time.timeScale = 0f; // sustabdo žaidimą
+
+        if (winPanel != null)
+            winPanel.SetActive(true);
     }
 }
